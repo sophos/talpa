ca03aa70ef60bca9c186d0fc3c3acd0af751c034
Tim Pickersgill <tim.pickersgill@sophos.com>
2017-09-19 14:19:53 +0000

    [LINUXEP-3633] Add talpa unit tests for mount and uts namespaces, bind and move mounts.
    [+DLCL]
    
    Merging //dev/talpa/main/tests/chk_chroot.c
    to //dev/talpa/main/tests/chk_namespace.c
    
    Merging //dev/talpa/main/tests/chk_chroot.sh
    to //dev/talpa/main/tests/chk_namespace.sh
    
    Merging //dev/talpa/main/tests/chk_chroot.c
    to //dev/talpa/main/tests/chk_container.c
    
    Merging //dev/talpa/main/tests/chk_chroot.sh
    to //dev/talpa/main/tests/chk_container.sh
    
    Merging //dev/talpa/main/tests/chk_null_fstype_mnt.c
    to //dev/talpa/main/tests/chk_bind1.c
    
    [git-p4: depot-paths = "//dev/talpa/main/": change = 2727614]

--- tests/chk_bind1.sh
+++ tests/chk_bind1.sh
@@ -15,33 +15,27 @@
 # write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 #
 
-. ${srcdir}/functions.sh
+. ${srcdir}/talpa-init.sh
 
-talpa_disable
-talpa_unload
+function get_count() { awk '{ sum += $2 } END { print sum }' $talpafs/interceptors/VFSHookInterceptor/fs-list; }
+function fail() { echo "$@"; exit 1; }
 
-# Wait for modules to unload
-for i in 0 1 2 3 4 5 6 7 8 9
-do
-    if lsmod | grep 'talpa\|tlp'
-    then
-        sleep 0.2 || sleep 1
-    else
-        break
-    fi
-done
+## fail on error
+set -e
 
-if [ -d /tmp/tlp-test ]
-then
-    LSOF=`which lsof 2>/dev/null`
-    if [ -x "$LSOF" ]
-    then
-        lsof /tmp/tlp-test 2>/dev/null
-    fi
-    mount | grep /tmp/tlp-test
-    [ -d /tmp/tlp-test/mnt1 ] && umount /tmp/tlp-test/mnt1 2>/dev/null
-    [ -d /tmp/tlp-test/mnt2 ] && umount /tmp/tlp-test/mnt2 2>/dev/null
-    </proc/mounts cut -f2 | grep /tmp/tlp-test | xargs -r umount 2>/dev/null
-    rm -rf /tmp/tlp-test 2>/dev/null
-fi
-true
+mkdir /tmp/tlp-test/a
+mkdir /tmp/tlp-test/b
+
+a=$(get_count)
+# mount with NULL fstype
+./chk_bind1 /tmp/tlp-test/a /tmp/tlp-test/b
+b=$(get_count)
+umount /tmp/tlp-test/b
+c=$(get_count)
+
+(( a > 1 )) || fail "failed to get usage count"
+(( a < b )) || fail "bind mount didn't increase usage count"
+(( c < b )) || fail "umount in shared area didn't decrease usage count"
+(( c == a )) || fail "usage count not restored after shared umount"
+
+exit 0
--- tests/chk_namespace.sh
+++ tests/chk_namespace.sh
@@ -15,33 +15,7 @@
 # write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 #
 
-. ${srcdir}/functions.sh
+. ${srcdir}/talpa-init.sh
+./chk_namespace 0 /tmp/tlp-test file
 
-talpa_disable
-talpa_unload
-
-# Wait for modules to unload
-for i in 0 1 2 3 4 5 6 7 8 9
-do
-    if lsmod | grep 'talpa\|tlp'
-    then
-        sleep 0.2 || sleep 1
-    else
-        break
-    fi
-done
-
-if [ -d /tmp/tlp-test ]
-then
-    LSOF=`which lsof 2>/dev/null`
-    if [ -x "$LSOF" ]
-    then
-        lsof /tmp/tlp-test 2>/dev/null
-    fi
-    mount | grep /tmp/tlp-test
-    [ -d /tmp/tlp-test/mnt1 ] && umount /tmp/tlp-test/mnt1 2>/dev/null
-    [ -d /tmp/tlp-test/mnt2 ] && umount /tmp/tlp-test/mnt2 2>/dev/null
-    </proc/mounts cut -f2 | grep /tmp/tlp-test | xargs -r umount 2>/dev/null
-    rm -rf /tmp/tlp-test 2>/dev/null
-fi
-true
+exit $?
